1d44b85c811163f031033fd9baad246162659e58
EnriqueA75 <48929949+EnriqueA75@users.noreply.github.com>
2021-07-22 17:15:39 -0600

    Making Hooks Reusable - Finished - Day End

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,37 +1,20 @@
-import React, { useState, useEffect }  from 'react';
+import React, { useState }  from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
 
     const [term, setTerm] = useState('')
-    const [results, setResults] = useState([])
-    const [errorMessage, setErrorMessage] = useState('')
+    const [searchApi, results, errorMessage] = useResults()
 
-    const searchApi = async (searchTerm) => {
-        try {
-            const response = await yelp.get('/search', {
-                params: {
-                    limit: 50,
-                    term: searchTerm,
-                    location: 'san jose'
-                }
-            })
-            setResults(response.data.businesses)
-        } catch (error) {
-            setErrorMessage('Something Went Wrong')
-
-            setTimeout(() => {
-               setErrorMessage('') 
-            }, 5000);
-        }
+    const filterResultsByPrice = (price) => {
+        //price $ || $$ || $$
+        return results.filter(result => {
+            return result.price === price
+        })
     }
-    //call the seaarchAPI when component render
-    useEffect(() => {
-        searchApi('pasta')
-    }, [])
-    
     return ( 
         <>
             <SearchBar 
@@ -39,11 +22,17 @@ const SearchScreen = () => {
                 setTerm={setTerm}
                 onTermSubmit={() => searchApi(term)}
             />
+
+            <ResultsList title={"Cost Effective"} results={filterResultsByPrice('$')}/>
+            <ResultsList title={"Bit Pricier"} results={filterResultsByPrice('$$')}/>
+            <ResultsList title={"Big Spender"} results={filterResultsByPrice('$$$')}/>
+
             <Text>We have found {results.length} results</Text>
             {errorMessage ? <View style={styles.errorContainer}><Text style={styles.errorText}>{errorMessage}</Text></View> : null}
         </>
      );
 }
+
 const styles = StyleSheet.create({
     errorContainer: {
         backgroundColor: '#E74C3C',
